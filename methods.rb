754c7f8532cc0aa004ab3d12ea0ce674968f37d2
Aaron Gooden <aagooden@me.com>
2018-05-10 17:54:14 -0400

    test_rover_class_direction

--- methods.rb
+++ methods.rb
@@ -1,10 +1,11 @@
 class Rover
 
-  attr_accessor :name, :position
+  attr_accessor :name, :position, :direction
 
-  def initialize(name, position="position")
+  def initialize(name, position="position", direction="direction")
     @name = name
     @position = position
+    @direction = direction
   end
 
 end

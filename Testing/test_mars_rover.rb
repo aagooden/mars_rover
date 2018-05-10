620ac502f8d89c461eb7bb976e7fc15014b8077b
Aaron Gooden <aagooden@me.com>
2018-05-10 16:03:28 -0400

    Rover class initialize test written and passing.

--- Testing/test_mars_rover.rb
+++ Testing/test_mars_rover.rb
@@ -1,10 +1,17 @@
 require "minitest/autorun"
 require_relative "../app.rb"
+require_relative "../methods.rb"
 
 class Mars_test < Minitest::Test
 
 	def test_boolean
 		assert_equal(true, true)
 	end
+
+  def test_rover_class_initialize
+    rover = Rover.new("rover1")
+    assert_equal("rover1", rover.name)
+    puts "#{rover.name}"
+  end
 
 end
--- methods.rb
+++ methods.rb
@@ -1,7 +1,9 @@
+class Rover
 
+attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
 
-
-
-
-
+end

3500d7f71eb19651523f24d1217cf8fbfff049a7
Aaron Gooden <aagooden@me.com>
2018-05-11 09:57:58 -0400

    test_set_grid_size failing

--- grid.rb
+++ grid.rb
@@ -2,9 +2,14 @@ class Grid
 
   attr_accessor :name, :size
 
-  def initialize(name, size=[1,1])
+  def initialize(name="grid", size=[1,1])
     @name = name
     @size = size
   end
+
+  def set_grid_size(information_array)
+  end
+
+
 
 end
--- rover.rb
+++ rover.rb
@@ -2,7 +2,7 @@ class Rover
 
   attr_accessor :name, :position, :direction
 
-  def initialize(name, position="position", direction="direction")
+  def initialize(name="rover1", position="position", direction="direction")
     @name = name
     @position = position
     @direction = direction

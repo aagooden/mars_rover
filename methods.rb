6a34b57078346422535d8e275c5a529dac722155
Aaron Gooden <aagooden@me.com>
2018-05-14 09:38:11 -0400

    controller_hash_creation_grid_size passing

--- methods.rb
+++ methods.rb
@@ -12,11 +12,16 @@ end
 def controller_hash_creation(instructions)
   instructions_hash = {}
   information_array = instructions_array(instructions)
-
+  p information_array
   return instructions_hash
 end
 
 def controller_hash_creation_grid_size(instructions)
+  grid_size = []
+  information_array = instructions_array(instructions)
+  grid_size[0] = information_array[0][0].to_i
+  grid_size[1] = information_array[0][2].to_i
+  return grid_size
 end
 
 def controller_hash_creation_rover_position(instructions, controller_hash)

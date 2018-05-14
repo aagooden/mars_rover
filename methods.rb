ba50e3323fc438359a3f3f5529190e78582cbfdc
Aaron Gooden <aagooden@me.com>
2018-05-14 10:58:17 -0400

    test_controller_hash_creation passing

--- methods.rb
+++ methods.rb
@@ -12,7 +12,12 @@ end
 def controller_hash_creation(instructions,number_of_rovers=2)
   instructions_hash = {}
   information_array = instructions_array(instructions)
-
+  puts "Information array is #{information_array}"
+  instructions_hash["grid_size"] = controller_hash_creation_grid_size(instructions)
+  for x in (1..number_of_rovers) do
+    instructions_hash["move#{x}"] = [controller_hash_creation_rover_position(information_array[2 * x - 1]), controller_hash_creation_rover_direction(information_array[2 * x - 1]), information_array[2 * x]]
+  end
+  puts instructions_hash
   return instructions_hash
 end
 

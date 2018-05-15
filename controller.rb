a8cf643363a335bee0328cfd8df20cc7a67a2b53
Aaron Gooden <aagooden@me.com>
2018-05-15 08:50:21 -0400

    test_controller_move_rovers_1 failing.

--- controller.rb
+++ controller.rb
@@ -5,17 +5,34 @@ class Controller
   def initialize(name="controller", controller_hash={}, number_of_rovers=2)
     @name = name
     @controller_hash = controller_hash
-    puts "This is controller hash move1 !!!!!!! #{controller_hash["move1"][0]}"
+    # puts "This is controller hash move1 !!!!!!! #{controller_hash["move1"][0]}"
+    @grid = Grid.new(controller_hash["grid_size"])
     @number_of_rovers = number_of_rovers
     @rovers = {}
     for x in (1..@number_of_rovers) do
-      puts "This is controller_hash #{controller_hash}"
+      # puts "This is controller_hash #{controller_hash}"
       @rovers["rover#{x}"] = Rover.new("rover#{x}", controller_hash["move#{x}"][0], controller_hash["move#{x}"][1], controller_hash["move#{x}"][2])
     end
   end
 
-  def move_rovers
-    routes = {}
-    return routes
+  def move_rovers(current_rover)
+    route_array = {}
+    # for x in (1..@rovers.length) do
+      rover_moves = current_rover.moves.each_char.to_a
+      rover_moves.each do |r_move|
+          case r_move
+            when "M"
+              new_coordinates = current_rover.move
+                if @grid.check_move(new_coordinates)
+                  route_array[current_rover.name].push(new_coordinates)
+                end
+          end
+          puts "Here are the new coordinates #{new_coordinates}"
+          puts "Here is the move_array #{route_array}"
+      end
+    # end
+    return route_array
   end
 end
+
+# {"rover1"=>[[1,2,"N"],[0,2,"W"],[0,1,"S"],[1,1,"E"],[1,2,"N"],[1,3,"N"]]}

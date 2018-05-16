c3a89d01f8cf7100f3a1ebcff9a65e3523727852
Aaron Gooden <aagooden@me.com>
2018-05-16 09:30:17 -0400

    test_grid_pac_man_move_4 passing

--- Testing/test_mars_rover.rb
+++ Testing/test_mars_rover.rb
@@ -316,11 +316,11 @@ class Mars_test < Minitest::Test
 		checked_move = grid.pac_man_move(possible_coordinates)
 		assert_equal([5,5], checked_move)
 	end
-	#
-	# def test_grid_pac_man_move_4
-	# 	grid = Grid.new("grid1",[5,5])
-	# 	possible_coordinates = [1,-1]
-	# 	checked_move = grid.check_move(possible_coordinates)
-	# 	assert_equal(false, checked_move)
-	# end
+
+	def test_grid_pac_man_move_4
+		grid = Grid.new("grid1",[5,5])
+		possible_coordinates = [1,-1]
+		checked_move = grid.pac_man_move(possible_coordinates)
+		assert_equal([1,5], checked_move)
+	end
 end

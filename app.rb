36eace77d9a4fe5abcf720bc94f5321d065e9144
Aaron Gooden <aagooden@me.com>
2018-05-16 14:01:45 -0400

    Grid dynamically drawn in mission view erb

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ end
 post "/mission_info" do
 	grid_size_x = params[:grid_x_value].to_i
 	grid_size_y = params[:grid_y_value].to_i
+	session[:grid_size] = [grid_size_x, grid_size_y]
 
 	rover1_start_x = params[:rover1_x_value].to_i
 	rover1_start_y = params[:rover1_y_value].to_i
@@ -22,14 +23,14 @@ post "/mission_info" do
 	rover2_direction = params[:rover2_direction]
 	rover2_moves = params[:rover2_moves]
 
-	number_of_rovers = 2
+	session[:number_of_rovers] = 2
 
 	instructions = "#{grid_size_x} #{grid_size_y}\n#{rover1_start_x} #{rover1_start_y} #{rover1_direction}\n#{rover1_moves}\n#{rover2_start_x} #{rover2_start_y} #{rover2_direction}\n#{rover2_moves}"
 	# instructions = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM"
-	controller_hash = controller_hash_creation(instructions,2)
-	session[:controller1] = Controller.new("controller1", controller_hash, number_of_rovers)
+	controller_hash = controller_hash_creation(instructions, session[:number_of_rovers])
+	session[:controller1] = Controller.new("controller1", controller_hash, session[:number_of_rovers])
 	rover_routes = {}
-	for x in (1..number_of_rovers) do
+	for x in (1..session[:number_of_rovers]) do
 		current_rover = session[:controller1].rovers["rover#{x}"]
 		rover_route = session[:controller1].move_rovers(current_rover)
 		rover_route = rover_route["rover#{x}"]
